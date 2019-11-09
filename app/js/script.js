5e9e84b9fc57d34862f96a5ec754c77e9a070a5b
Barsukov Nikolay <nb721@mail.ru>
2019-11-09 15:01:37 +0700

    JS Lesson 1

---
+++ app/js/script.js
@@ -0,0 +1,25 @@
+const elements = document.querySelectorAll("*");
+
+const genIndex = (base) => Math.floor(base*Math.random());
+
+const elIndex = genIndex(elements.length);
+const indexChanged = [elIndex];
+
+const genColor = () =>`rgb(${Math.random()*256},${Math.random()*256},${Math.random()*256}`;
+
+elements[elIndex].style.backgroundColor = genColor();
+
+const firstInterval = setInterval(()=>{
+    const indexChange = genIndex(elements.length);
+    indexChanged.push(indexChange);
+    elements[indexChange].style.backgroundColor = genColor();
+},200);
+
+
+const secondInterval = setInterval(()=>{
+    if (indexChanged.length >0){
+        const indexDefault = genIndex(indexChanged.length);
+        elements[indexChanged.splice(indexDefault,1)].style.backgroundColor="";
+    }
+}, 400)
+
