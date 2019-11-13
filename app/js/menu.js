47083d37ea3fee81c2bc4117d2fcab945ddba408
Kursant <Kursant@BOOK508wm8.win.ftc.ru>
2019-11-13 21:43:00 +0700

    Added hover menu for category.html

---
+++ app/js/menu.js
@@ -0,0 +1,306 @@
+const menuContent = [
+	{
+		heading:"Home decor",
+		subcats: [
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+		]
+	},
+	{
+		heading:"Furniture",
+		subcats: [
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+		]
+	},
+	{
+		heading:"Lighting",
+		subcats: [
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+		]
+	},
+	{
+		heading:"Home Accents",
+		subcats: [
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+		]
+	},
+	{
+		heading:"Rugs",
+		subcats: [
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+		]
+	},
+	{
+		heading:"Outdoors",
+		subcats: [
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+		]
+	},
+	{
+		heading:"Holiday",
+		subcats: [
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+		]
+	},
+	{
+		heading:"Gifts",
+		subcats: [
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+		]
+	},
+	{
+		heading:"Events",
+		subcats: [
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+			{name:"Subcategory", link:"#"},
+		]
+	}
+]
+
+const menus = document.querySelectorAll(".c-navbar__item"),
+	menuTemplate = document.querySelector(".menu-template");
+
+const addSubcat = (subcat, element) => {
+	const subcategory = document.createElement("a");
+	subcategory.setAttribute("class","menu__item");
+	subcategory.setAttribute("href",`${element.link}`);
+	subcategory.innerText = `${element.name}`;
+	subcat.appendChild(subcategory);
+};
+
+const renderMenus = (menu, shift, index) => {
+	menu.appendChild(document.importNode(menuTemplate.content, true));
+	const menuHeadingText = menu.querySelector(".menu__main"),
+		menuBody = menu.querySelector(".menu");
+	menuHeadingText.innerText = menuContent[index].heading,
+	menuSubcats = menu.querySelectorAll(".menu__subcats");
+	menuContent[index].subcats.forEach((element)=>{
+		if(menuSubcats[0].querySelectorAll(".menu__item").length<8){
+			addSubcat(menuSubcats[0], element);
+			
+		} else if(menuSubcats[1].querySelectorAll(".menu__item").length<8){
+			addSubcat(menuSubcats[1], element);
+		} else if (menuSubcats[2].querySelectorAll(".menu__item").length<8){
+			addSubcat(menuSubcats[2], element);
+		} else {
+			console.log ("Cannot visualise element", element.name);
+		}
+	});
+	menuBody.style.left = `calc((100% - 84.1rem) / 2 + ${shift}px + 3rem*${index})`;	
+};
+
+
+const showMenu = function (event) {
+	const menuObj = event.target.querySelector(".menu").classList;
+	if (menuObj.contains("menu_hidden")) menuObj.remove("menu_hidden");
+}
+
+const hideMenu = function (event) {	
+	const menuObj = event.target.querySelector(".menu").classList;
+	if(!menuObj.contains("menu_hidden")) menuObj.add("menu_hidden");
+}
+
+let shift = 0, indexMenus = 0;
+
+menus.forEach(element => {
+	renderMenus(element, shift, indexMenus++);
+	shift = shift + element.offsetWidth;
+	element.addEventListener("mouseenter", showMenu);
+	element.addEventListener("mouseleave", hideMenu);
+});
