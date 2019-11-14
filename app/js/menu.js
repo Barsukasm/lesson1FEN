2c8828a02290836f220c09d77182f9474daccc82
Barsukasm <nb721@mail.ru>
2019-11-14 18:59:11 +0700

    Added menu for both pages. Fixed submenu paddings

--- app/js/menu.js
+++ app/js/menu.js
@@ -268,20 +268,25 @@ const renderMenus = (menu, shift, index) => {
 	menu.appendChild(document.importNode(menuTemplate.content, true));
 	const menuHeadingText = menu.querySelector(".menu__main"),
 		menuBody = menu.querySelector(".menu");
-	menuHeadingText.innerText = menuContent[index].heading,
+	let subcatsWidth = [0, 0, 0];
+	menuHeadingText.innerText = menuContent[index].heading;
 	menuSubcats = menu.querySelectorAll(".menu__subcats");
 	menuContent[index].subcats.forEach((element)=>{
 		if(menuSubcats[0].querySelectorAll(".menu__item").length<8){
 			addSubcat(menuSubcats[0], element);
-			
+			if (element.name.length>subcatsWidth[0]) subcatsWidth[0] = element.name.length;
 		} else if(menuSubcats[1].querySelectorAll(".menu__item").length<8){
 			addSubcat(menuSubcats[1], element);
+			if (element.name.length>subcatsWidth[1]) subcatsWidth[1] = element.name.length;
 		} else if (menuSubcats[2].querySelectorAll(".menu__item").length<8){
 			addSubcat(menuSubcats[2], element);
+			if (element.name.length>subcatsWidth[2]) subcatsWidth[2] = element.name.length;
 		} else {
 			console.log ("Cannot visualise element", element.name);
 		}
 	});
+
+	menuBody.style.width = `calc(3rem + ${subcatsWidth.reduce((acc, cur) => cur>0? acc +1: acc, 0)}*3rem + ${subcatsWidth.reduce((acc, cur) => acc + cur, 0)}*7.75px)`;
 	menuBody.style.left = `calc((100% - 84.1rem) / 2 + ${shift}px + 3rem*${index})`;	
 };
 
@@ -289,18 +294,24 @@ const renderMenus = (menu, shift, index) => {
 const showMenu = function (event) {
 	const menuObj = event.target.querySelector(".menu").classList;
 	if (menuObj.contains("menu_hidden")) menuObj.remove("menu_hidden");
-}
+};
 
 const hideMenu = function (event) {	
 	const menuObj = event.target.querySelector(".menu").classList;
 	if(!menuObj.contains("menu_hidden")) menuObj.add("menu_hidden");
-}
+};
 
-let shift = 0, indexMenus = 0;
+const hoverMenu = (menus) => {
+	let shift = 0, indexMenus = 0;
 
-menus.forEach(element => {
-	renderMenus(element, shift, indexMenus++);
-	shift = shift + element.offsetWidth;
-	element.addEventListener("mouseenter", showMenu);
-	element.addEventListener("mouseleave", hideMenu);
-});
+	menus.forEach(element => {
+		renderMenus(element, shift, indexMenus++);
+		shift = shift + element.offsetWidth;
+		element.addEventListener("mouseenter", showMenu);
+		element.addEventListener("mouseleave", hideMenu);
+	});
+};
+
+hoverMenu(menus);
+
+
